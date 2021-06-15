143f9794d17262829cfbeeb6e1028f29084bb565
Vijaya Banka <vijay.bhaskar@macys.com>
2021-06-14 19:04:41 -0700

    Adding files

---
+++ BasicJava/src/com/rechingskies/basicjava/introduction/VariablesAndDataTypes.java
@@ -0,0 +1,17 @@
+package com.rechingskies.basicjava.introduction;
+
+public class VariablesAndDataTypes {
+
+    public static void main(String [] args) {
+        boolean isValid = true;
+        byte marks = 90;
+        float pi = 3.14f;
+        double weight = 190.20; //190.20d
+        int age = 40;
+        long views = 10000;
+        char gender = 'M';
+
+        System.out.println("Marks : "+marks+" "+" PI : "+pi+" "+" Weight : "+weight+" Age : "+age+" Views : "+
+                views+" Gender "+gender);
+    }
+}
