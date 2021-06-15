e70595847f11ee39b675d1f267c3e76a214c3996
Vijaya Banka <vijay.bhaskar@macys.com>
2021-06-14 18:22:40 -0700

    Adding files

---
+++ BasicJava/src/com/rechingskies/basicjava/introduction/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.rechingskies.basicjava.introduction;
+
+public class HelloWorld {
+
+    public static void main(String [] args) {
+        System.out.println("Welcome to CIP course...");
+    }
+}
